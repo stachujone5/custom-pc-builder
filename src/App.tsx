b65f150efe0b87721b067cd828be7d0981a85d65
Stanislaw Jakubowski <sjakubowski3554@gmail.com>
2022-04-06 18:09:42 +0200

    Finished basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import classes from './App.module.scss'
 import { Form } from './components/form/Form'
 import { Hero } from './components/hero/Hero'
+import {AiOutlineShoppingCart} from '
 
 export const App = () => {
 	return (
-		<main className={classes.main}>
-			<Hero />
-			<div className={classes['form-body']}>
-				<Form />
-			</div>
-		</main>
+		<>
+			<main className={classes.main}>
+				<Hero />
+				<div className={classes['form-body']}>
+					<Form />
+				</div>
+			</main>
+			<AiOutlineShoppingCart />
+		</>
 	)
 }
